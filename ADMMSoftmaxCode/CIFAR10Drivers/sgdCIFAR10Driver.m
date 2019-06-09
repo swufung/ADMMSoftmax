6071a92a7902f75f0286df9b63f4d2c888141f49
Samy Wu Fung <samy.wu001@gmail.com>
2019-06-09 11:31:46 -0400

    Update sgdCIFAR10Driver.m

--- ADMMSoftmaxCode/CIFAR10Drivers/sgdCIFAR10Driver.m
+++ ADMMSoftmaxCode/CIFAR10Drivers/sgdCIFAR10Driver.m
@@ -1,24 +1,27 @@
 clear all; 
 
-addpath(genpath('~/ADMMSoftmaxCode/')); 
+addpath(genpath('~/ADMMSoftmaxCode/')); % euler
 
-layer = 'pool5'; N      = 50000; Nval = 0.2*N;
-[Dtrain,Ctrain,Dval,Cval] = setupCIFAR10AlexNet(N, Nval, layer);
-Dtrain = double(Dtrain); Dval = double(Dval);
-nf = size(Dtrain,1) 
-nc = size(Ctrain,1) 
+N = 50000; Ntrain = 0.8*N; Nval = 0.2*N; Ntest = 0.2*N;
+
+layer = 'pool5';
+[Dtrain,Ctrain,Dval,Cval,Dtest,Ctest] = setupCIFAR10AlexNet(N, Ntest, layer);
+Dtrain = double(Dtrain); Dval = double(Dval); Dtest = double(Dtest);
+nf = size(Dtrain,1); 
+nc = size(Ctrain,1);
 
 fprintf('maxDtrain = %1.2e, minDtrain = %1.2e\n', max(Dtrain(:)), min(Dtrain(:)));
 fprintf('maxDval = %1.2e, minDval = %1.2e\n', max(Dval(:)), min(Dval(:)));
 
 Dtrain    = normalizeData(Dtrain, size(Dtrain,1));
 Dval      = normalizeData(Dval, size(Dval,1));
+Dtest     = normalizeData(Dtest, size(Dtest,1));
 fprintf('maxDtrain = %1.2e, minDtrain = %1.2e\n', max(Dtrain(:)), min(Dtrain(:)));
 fprintf('maxDval = %1.2e, minDval = %1.2e\n', max(Dval(:)), min(Dval(:)));
 
 %% regularization
 
-alpha = 1;
+alpha = 1e-1;
 
 addBias=true;
 % % % % nImg = [27 27]; channelsOut = 96; % pool1
@@ -45,7 +48,7 @@ fprintf(' max of Lout... ');
 max(Lout(:))
 fprintf('length of W = %d...\n', nf*nc)
 
-lr0 = 1e-3;
+lr0 = 1e-2;
 miniBatch=300;
 
 fprintf('\n\n lr0 = %1.2e, ALPHA = %1.2e, minibatch = %d \n', lr0, alpha, miniBatch);
@@ -54,38 +57,76 @@ fprintf('\n\n lr0 = %1.2e, ALPHA = %1.2e, minibatch = %d \n', lr0, alpha, miniBa
 pRegW   = tikhonovReg(Lout,alpha);
 pLoss   = softmaxLoss();
 f       = classObjFctn(pLoss,pRegW,Dtrain,Ctrain);
-fTest   = classObjFctn(pLoss,pRegW,Dval,Cval);
+fVal   = classObjFctn(pLoss,pRegW,Dval,Cval);
 
-f.pLoss.addBias=addBias; fTest.pLoss.addBias=addBias;
+f.pLoss.addBias=addBias; fVal.pLoss.addBias=addBias;
 
 vec     = @(x) x(:);
 
 if addBias==false
-    w      = vec(zeros(nf,nc));
+    w0      = vec(zeros(nf,nc));
 else
-    W0      = vec(zeros(nf+1,nc));
+    w0      = vec(zeros(nf+1,nc));
 end
 
 %% sgd setup
-opt           = sgd('out',1);
+opt           = sgdSoftmax('out',1);
 opt.nesterov=false;
 opt.rtol      = 1e-3;
 opt.atol      = 1e-3;
 opt.miniBatch = miniBatch;
 opt.maxEpochs = 100;
+% opt.learningRate = @(epoch) lr0/sqrt(epoch);
 opt.learningRate = lr0;
 opt.stoppingTime = 300; %seconds
 
 
-%% solve
+%% train
 tSolve = tic
-[wOpt, hisOpt] = solve(opt,f,W0, fTest);
+[wFinal, wOptLoss, wOptAcc, hisOpt] = solve(opt,f,w0, fVal);
 tSolve = toc(tSolve)
 
-fprintf('\n\n ALPHA = %1.2e, fTrain = %1.2e, fTest = %1.2e, trainAcc = %1.2f, testAcc=%1.2f\n\n', ...
-    alpha, hisOpt.his(end,9), hisOpt.his(end,13), hisOpt.his(end,10), hisOpt.his(end,13)); %14 for newton
+%% save best validation, and testing loss and accuracy values
+% weights that minimize validation misfit on validation data
+pLossVal = softmaxLoss();
+fVal2    = classObjFctn(pLossVal,pRegW,Dval,Cval);
 
-atol = opt.atol; rtol = opt.rtol; 
-miniBatch = opt.miniBatch;
-    
-save('sgdResultsCIFAR10.mat', 'hisOpt', 'wOpt', 'alpha', 'atol', 'miniBatch', 'rtol', 'lr0', 'miniBatch')
+[FcValLoss, paraValLoss] = fVal2.eval(wOptLoss(:));
+errValLoss = paraValLoss.hisLoss(3);
+accValLoss = 100*(Nval-errValLoss)/Nval;
+
+% weights that minimize validation misfit on validation data
+[FcValAcc, paraValAcc] = fVal2.eval(wOptAcc(:));
+errValAcc = paraValAcc.hisLoss(3);
+accValAcc = 100*(Nval-errValAcc)/Nval;
+
+fprintf('\n ------ VALIDATION RESULTS ------ \n')
+fprintf('\n wOptLoss: fVal = %1.2e, accVal = %1.2f\n', FcValLoss, accValLoss);
+fprintf('\n wOptAcc: fVal = %1.2e, accVal = %1.2f\n', FcValAcc, accValAcc);
+
+
+% testing dataset
+pLossTest = softmaxLoss();
+fTest     = classObjFctn(pLossTest,pRegW,Dtest,Ctest);
+
+% weights that minimize validation misfit on testing data
+[FcTestLoss, paraTestLoss] = fTest.eval(wOptLoss(:));
+errTestLoss = paraTestLoss.hisLoss(3);
+accTestLoss = 100*(Ntest-errTestLoss)/Ntest;
+
+% weights that minimize validation misfit on testing data
+[FcTestAcc, paraTestAcc] = fTest.eval(wOptAcc(:));
+errTestAcc = paraTestAcc.hisLoss(3);
+accTestAcc = 100*(Ntest-errTestAcc)/Ntest;
+
+fprintf('\n ------ TESTING RESULTS ------ \n')
+fprintf('\n wOptLoss: fTest = %1.2e, accTest = %1.2f\n', FcTestLoss, accTestLoss);
+fprintf('\n wOptAcc: fTest = %1.2e, accTest = %1.2f\n', FcTestAcc, accTestAcc);
+
+
+atol = opt.atol; rtol = opt.rtol; miniBatch = opt.miniBatch;
+
+saveResults = 0;
+if saveResults==1
+    save('sgdResultsCIFAR10.mat', 'hisOpt', 'wFinal', 'wOptLoss', 'wOptAcc', 'alpha', 'atol', 'miniBatch', 'rtol', 'lr0', 'miniBatch', 'FcTestLoss', 'accTestLoss', 'FcTestAcc', 'accTestAcc')
+end
